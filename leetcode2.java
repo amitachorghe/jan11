7a7271a7fcbc5b01203293351ac13c63fb10d35b
Amita  chorghe <67407962+amitachorghe@users.noreply.github.com>
2022-01-16 19:23:03 +0530

    Add files via upload

---
+++ leetcode2.java
@@ -0,0 +1,20 @@
+public class leetcode2 {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode temp = head;
+
+        while (temp != null && temp.next != null) {
+            if (temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+                continue;
+            }
+            temp = temp.next;
+        }
+
+        return head;
+    }
+}
